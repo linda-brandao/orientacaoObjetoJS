8788bb9b92f21d1ce150abae29ea42b6688f85a0
Linda Brandão <linda.15brandao@gmail.com>
2023-07-27 09:24:52 -0400

    criando instâncias das classes User e Admin

---
+++ index.js
@@ -0,0 +1,6 @@
+import User from "./User.js";
+import Admin from "./Admin.js";
+import Docente from "./Docente.js";
+
+const novoUser = new User('Karina', 'k@k.com', '2001-01-01');
+const novoAdmin = new Admin('Mario', 'm@m.com', '2025-05-02');
