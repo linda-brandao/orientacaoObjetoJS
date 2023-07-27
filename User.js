c59b72aada0a9870be4f5fb6705236c311d325e5
Linda Brandão <linda.15brandao@gmail.com>
2023-07-27 09:24:07 -0400

    tornando os atributos privados e utilizando os métodos getters e setters

--- User.js
+++ User.js
@@ -1,10 +1,47 @@
 export default class User {
+    #nome
+    #email
+    #nascimento
+    #role
+    #ativo
     constructor(nome, email, nascimento, role = 'estudante', ativo = true) {
-        this.nome = nome;
-        this.email = email;
-        this.nascimento = nascimento;
-        this.role = role;
-        this.ativo = ativo;
+        this.#nome = nome;
+        this.#email = email;
+        this.#nascimento = nascimento;
+        this.#role = role;
+        this.#ativo = ativo;
+    }
+
+    get nome() {
+        return this.#nome;
+    }
+    get email() {
+        return this.#email;
+    }
+    get nascimento() {
+        return this.#nascimento;
+    }
+    get role() {
+        return this.#role;
+    }
+    get ativo() {
+        return this.#ativo;
+    }
+
+    set nome(nome) {
+        this.#nome = nome;
+    }
+    set email(email) {
+        this.#email = email;
+    }
+    set nascimento(nascimento) {
+        this.#nascimento = nascimento;
+    }
+    set role(role) {
+        this.#role = role;
+    }
+    set ativo(ativo) {
+        this.#ativo = ativo;
     }
 
     exibiInfo() {
